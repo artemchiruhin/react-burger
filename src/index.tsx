1c4f7a83b6f1ef5367ff57f6defb635581fa03fd
Chirukhin Artem <artemchiruhin@bk.ru>
2024-02-17 15:09:22 +0500

    добавляет импорт шрифтов

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './fonts/JetBrainsMono-Bold.ttf';
+import './fonts/JetBrainsMono-Medium.ttf';
+import './fonts/JetBrainsMono-Regular.ttf';
+import './fonts/JetBrainsMono-SemiBold.ttf';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
