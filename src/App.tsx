b3e5dd5cd56be9a1f786795ddbd18bbf82684d97
Chirukhin Artem <artemchiruhin@bk.ru>
2024-02-18 14:47:05 +0500

    меняет передачу тестовых данных в компонент конструктора

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,7 @@ function App() {
             <main className="main">
                 <div className="container main-container">
                     <BurgerIngredients ingredients={data} />
-                    <BurgerConstructor ingredients={[{
-                        "_id":"60666c42cc7b410027a1a9ba",
-                        "name":"Соус с шипами Антарианского плоскоходца",
-                        "type":"sauce",
-                        "proteins":101,
-                        "fat":99,
-                        "carbohydrates":100,
-                        "calories":100,
-                        "price":88,
-                        "image":"https://code.s3.yandex.net/react/code/sauce-01.png",
-                        "image_mobile":"https://code.s3.yandex.net/react/code/sauce-01-mobile.png",
-                        "image_large":"https://code.s3.yandex.net/react/code/sauce-01-large.png",
-                        "__v":0
-                    }]} />
+                    <BurgerConstructor ingredients={[data[0]]} />
                 </div>
             </main>
         </>
