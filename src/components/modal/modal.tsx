63795f73211771a84094336c3fced5778a0dd338
Chirukhin Artem <artemchiruhin@bk.ru>
2024-02-23 14:46:09 +0500

    добавляет компоненты модальных окон

---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,63 @@
+import React, {useEffect} from 'react';
+import {CloseIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './modal.module.css';
+
+interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
+    onClose?: () => void,
+}
+
+export const Modal = ({ children, className, onClose, ...props }: ModalProps) => {
+    useEffect(() => {
+        document.addEventListener('keydown', (event) => {
+            if(event.key !== 'Escape') return;
+            onClose && onClose();
+        });
+
+        return () => document.removeEventListener('keydown', (event) => {
+            if(event.key !== 'Escape') return;
+            onClose && onClose();
+        });
+    }, []);
+
+    return (
+        <div className={`${styles['modal']} ${className}`} {...props}>
+            { children }
+        </div>
+    );
+}
+
+interface ModalContentProps extends React.HTMLAttributes<HTMLDivElement> {
+
+}
+
+Modal.Content = ({ children, className, ...props }: ModalContentProps) => {
+    return (
+        <div className={`${styles['modal-content']} ${className}`} {...props}>
+            { children }
+        </div>
+    );
+}
+
+interface ModalTitleProps extends React.HTMLAttributes<HTMLDivElement> {
+
+}
+
+Modal.Title = ({ children, ...props }: ModalTitleProps) => {
+    return (
+        <div {...props}>
+            { children }
+        </div>
+    );
+}
+
+interface ModalCloseButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+
+}
+
+Modal.CloseButton = ({ ...props }: ModalCloseButtonProps) => {
+    return (
+        <button className={`${styles['close']}`} {...props}>
+            <CloseIcon type="primary" />
+        </button>
+    );
+}
